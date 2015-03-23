a9aa09730484e81067ed14fd882cf40b83567323
Alexey Ivanov <ivanov@intspirit.com>
2015-03-23 13:46:26 +0300

    initial commit

---
+++ install_plugins.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ionic platform rm ios
+ionic platform rm android
+
+rm -rf plugins/*
+
+ionic plugin add org.apache.cordova.media
+ionic plugin add org.apache.cordova.device
+ionic plugin add org.apache.cordova.file
+
+ionic platform add ios
+ionic platform add android
